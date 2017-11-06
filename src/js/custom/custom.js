15336c560ac9173d3fe6530fb7f6750c274072e8
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-06 17:21:50 +0200

    remove localsoradge

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -2,11 +2,4 @@ console.info("home work 5 v1.1");
 //https://codepen.io/xmark/pen/ogGXmP
 //https://www.youtube.com/watch?v=2wCpkOk2uCg
 
-var data = (localStorage.getItem('todoList')) ? JSON.parse(localStorage.getItem('todoList')):{
-      todo: [],
-      all: [],
-      completed: []
-    };
 
-var removeSVG = '<i class="fa fa-check" aria-hidden="true"></i>'
-var completeSVG = '<i class="fa fa-trash" aria-hidden="true"></i>'
