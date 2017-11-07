f0e3f8d85e346351913dba4b2b1288de77010d99
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-07 07:06:28 +0200

    add all render list function

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -3,7 +3,6 @@ console.info("home work 5 v1.1");
 //https://www.youtube.com/watch?v=2wCpkOk2uCg&t=3111shttps://www.youtube.com/watch?v=2wCpkOk2uCg&t=3111s
 
 
-
 var data = {
   todo: [],
   all: [],
@@ -11,23 +10,43 @@ var data = {
 
 }
 
-function addItem (value) {
+function addItem(value) {
   document.getElementById('item').value = '';
   data.todo.push(value);
 }
 
-document.getElementById('add').addEventListener('click', function() {
+document.getElementById('add').addEventListener('click', function () {
   var value = document.getElementById('item').value;
   if (value) addItem(value);
 });
 
-function renderTodoList() {
-  if (!data.todo.length ) {
+function renderToDoList() {
+  if (!data.todo.length) {
     for (var i = 0; i < data.todo.length; i++) {
       var value = data.todo[i];
+
     }
   }
 }
+
+function renderCompletedList() {
+  if (!data.completed.length) {
+    for (var j = 0; j < data.completed.length; j++) {
+      var value = data.completed[j];
+
+    }
+  }
+}
+
+function renderAllList() {
+  if (!data.all.length) {
+    for (var k = 0; k < data.all.length; k++) {
+      var value = data.all[k];
+
+    }
+  }
+}
+
 
 
 
