332e1e51022c23516d268fb3413ed756d2003bdf
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-07 07:00:07 +0200

    check todo list

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -2,11 +2,13 @@ console.info("home work 5 v1.1");
 //https://codepen.io/xmark/pen/ogGXmP
 //https://www.youtube.com/watch?v=2wCpkOk2uCg&t=3111shttps://www.youtube.com/watch?v=2wCpkOk2uCg&t=3111s
 
-var i;
-i = 1;
+
 
 var data = {
-  todo: []
+  todo: [],
+  all: [],
+  completed: []
+
 }
 
 function addItem (value) {
@@ -16,7 +18,16 @@ function addItem (value) {
 
 document.getElementById('add').addEventListener('click', function() {
   var value = document.getElementById('item').value;
-  addItem(value);
-
+  if (value) addItem(value);
 });
+
+
+  if (!data.todo.length ) {
+    for (var i = 0; i < data.todo.length; i++) {
+      var value = data.todo[i];
+    }
+  }
+
+
+
 
