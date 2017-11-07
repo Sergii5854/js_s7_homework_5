9f25590532c3927f970c5020b85725c669699df6
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-11-07 06:48:19 +0200

    add item to array

--- src/js/custom/custom.js
+++ src/js/custom/custom.js
@@ -1,42 +1,22 @@
 console.info("home work 5 v1.1");
 //https://codepen.io/xmark/pen/ogGXmP
-//https://www.youtube.com/watch?v=2wCpkOk2uCg
+//https://www.youtube.com/watch?v=2wCpkOk2uCg&t=3111shttps://www.youtube.com/watch?v=2wCpkOk2uCg&t=3111s
 
 var i;
 i = 1;
 
-function addText(id) {
-  var text = document.getElementById("div" + id).value;
-  text = text.slice(0,10 );
-  document.getElementById(id).querySelector("a").innerHTML= text;
-  /*document.getElementById(id).innerHTML = text + "...";*/
-};
+var data = {
+  todo: []
+}
+
+function addItem (value) {
+  document.getElementById('item').value = '';
+  data.todo.push(value);
+}
 
 document.getElementById('add').addEventListener('click', function() {
-  var newLi = document.createElement("li");
-  var a = document.createElement("a");
-  var newLiText = document.createTextNode("Hовая заметка " + i );
+  var value = document.getElementById('item').value;
+  addItem(value);
 
-  a.appendChild(newLiText);
-  newLi.appendChild(a);
-  /*console.log(newLi);*/
-  a.setAttribute("href","#" + "div" + i);
-  /*a.setAttribute("id", "a"+ i);*/
-  newLi.setAttribute("id", i);
-  document.getElementById("all").appendChild(newLi);
-  //
-  var newButton = document.createElement("button");
-  var buttonTrash = document.createTextNode('')
-
-  newButton.appendChild(buttonTrash);
-  newLi.appendChild(newButton);
-  newButton.setAttribute("onclick", "Del(" + i + ")");
-  newButton.innerHTML= '<i class="fa fa-trash" aria-hidden="true"></i>';
-
-  i++;
 });
 
-function Del(i){
-  document.getElementById(i).remove();
-  document.getElementById("div" + i).remove();
-}
